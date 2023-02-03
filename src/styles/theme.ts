4bc8d91a067fc74afab3854726f1c2bb9d7eb1a6
OJailson17 <JaylsonO17@gmail.com>
2023-02-03 00:28:30 -0300

    chore: setup chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+	styles: {
+		global: {
+			body: {
+				background: '#191919',
+				color: '#fff',
+				fontFamily: "'Noto Serif', serif",
+			},
+		},
+	},
+});
+
+// blue
+// #3B519D
+
+// span
+// #272727
