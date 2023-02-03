39fc0b4299d1d30e301b489cc79a3d4ad1263387
OJailson17 <JaylsonO17@gmail.com>
2023-02-03 00:28:51 -0300

    chore: add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { Box, Text } from '@chakra-ui/react';
+
+export const Header = () => {
+	return (
+		<Box
+			paddingY={['6']}
+			paddingX={['16']}
+			bg={'#272727'}
+			textAlign='center'
+			fontWeight={'bold'}
+		>
+			<Text>JO Dictionary</Text>
+		</Box>
+	);
+};
