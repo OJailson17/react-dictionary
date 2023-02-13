1dac630e7f8e038db65ee65ee6b116f6850f71eb
OJailson17 <JaylsonO17@gmail.com>
2023-02-12 21:14:30 -0300

    chore: remove react query tool

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 import { ChakraProvider } from '@chakra-ui/react';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { theme } from './styles/theme';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WordContextProvider } from './context/wordContext';
 
 // Create a client
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<QueryClientProvider client={queryClient}>
-		<ReactQueryDevtools initialIsOpen={false} />
 		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
